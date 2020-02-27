689bcd5a78b61943e33f267bf602214da705513a
bhalani97 <bhalanidivyesh@gmail.com>
2020-02-27 21:27:55 +0530

    second

--- src/main/java/com/rest/webservices/restfulwebservices/exception/ExceptionResponse.java
+++ src/main/java/com/rest/webservices/restfulwebservices/exception/ExceptionResponse.java
@@ -1,5 +1,29 @@
 package com.rest.webservices.restfulwebservices.exception;
 
+import java.util.Date;
+
 public class ExceptionResponse {
+	private String message;
+	private String details;
+	private Date timestamp;
+
+	public Date getTimestamp() {
+		return timestamp;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public String getDetails() {
+		return details;
+	}
+
+	public ExceptionResponse(Date timestamp, String message, String details) {
+		super();
+		this.timestamp = timestamp;
+		this.message = message;
+		this.details = details;
+	}
 
 }
